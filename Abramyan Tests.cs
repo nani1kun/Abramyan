054cb387a88020501555fe8acbe850b41b45623a
nani1kun <58101891+nani1kun@users.noreply.github.com>
2021-11-22 22:17:53 +0200

    Add files via upload

---
+++ Abramyan Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Abramyan;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace abrTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Begin1_Test_20_res80()
+        {
+            int x = 20;
+            int expected = x*4;
+            var c = new Abbr();
+            int actual = c.Begin1(x);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
