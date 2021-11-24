f1225a1366f5122b799d7c6e31720e0c8bf7017a
nani1kun <58101891+nani1kun@users.noreply.github.com>
2021-11-24 21:48:37 +0200

    Add files via upload

--- Abramyan Tests.cs
+++ Abramyan Tests.cs
@@ -15,5 +15,23 @@ namespace abrTest
             int actual = c.Begin1(x);
             Assert.AreEqual(expected, actual);
         }
+        public void Proc18_Test_10_res314()
+        {
+            double x = 10;
+            double expected = x*x*3.14;
+            var c = new Abbr();
+            double actual = c.Proc18(x);
+            Assert.AreEqual(expected, actual);
+        }
+        
+        public void Proc21_Test_10_15_res75()
+        {
+            int x = 10;
+            int y = 15;
+            double expected = 75;
+            var c = new Abbr();
+            int actual = c.Proc21(x,y);
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
